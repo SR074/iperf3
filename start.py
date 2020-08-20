287022a054d9487499d033e353f6ef668f40b64b
darksilky <ritexi.para@gmail.com>
2020-08-20 12:08:36 +0900

    test

--- start.py
+++ start.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta, timezone
+
 import configparser
 import subprocess
 import json
@@ -11,21 +13,33 @@ subprocess.run(
      "--logfile", "temp.json", "-J"])
 
 json_open = open('temp.json', 'r')
-json_load = json.load(json_open)
+res_json = json.load(json_open)
 
 config = {
     'user': config_ini['mysql']['User'],
     'password': config_ini['mysql']['Password'],
     'host': config_ini['mysql']['Host'],
-    'port':config_ini['mysql']['Port'],
+    'port': config_ini['mysql']['Port'],
     'database': config_ini['mysql']['Database'],
 }
 cnx = mysql.connector.connect(**config)
 cnx.ping(reconnect=True)
 cursor = cnx.cursor()
 
-get_results_last_id = select *
-push_id = get_results_last_id + 1
-cursor.
+host_id = config_ini['mysql']['HostId']
+remote_id = config_ini['mysql']['RemoteId']
 
+send_bytes = res_json['end']['sum_sent']['bytes']
+send_bps = res_json['end']['sum_sent']['bits_per_second']
+receive_bytes = res_json['end']['sum_received']['bytes']
+receive_bps = res_json['end']['sum_received']['bits_per_second']
+cpu_util_host = res_json['end']['cpu_utilization_percent']['host_total']
+cpu_util_remote = res_json['end']['cpu_utilization_percent']['remote_total']
+timestamp = res_json['start']['timestamp']['time']
+created = datetime.now(timezone(timedelta(hours=+9), 'JST'))
 
+sql = 'insert into results (host_id, remote_id, send_bytes, send_bps, receive_bytes, receive_bps, cpu_util_host, cpu_util_remote, timestamp, created) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+cursor.execute(sql, host_id, remote_id, send_bytes,send_bps,receive_bytes,receive_bps,cpu_util_host,cpu_util_remote,timestamp,created)
+
+cursor.close()
+cnx.close()
